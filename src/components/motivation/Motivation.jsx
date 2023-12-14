e59e7209a21111c450577978f1b95420d53ac6c8
Ghaith AlMasri <almasrighaith101@gmail.com>
2023-12-15 00:37:41 +0100

    done

--- src/components/motivation/Motivation.jsx
+++ src/components/motivation/Motivation.jsx
@@ -1,20 +1,20 @@
 "use client";
-import React, { useRef, useEffect } from "react";
-import { useInView } from "framer-motion";
+import React, { useRef } from "react";
+import { motion,useInView } from "framer-motion";
 
 const motivation = () => {
   const ref = useRef(null);
   const isInView = useInView(ref); // Destructure ref and inView from useInView()
 
   return (
-    <div className="bg-[#ffffff] flex justify-center items-center p-20">
+    <div className="bg-[#ffffff] flex justify-center items-center flex-col p-20">
       <div
         ref={ref}
         className="w-[90%] p-10 bg-[#e4e4e4] shadow-[rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset] rounded-2xl"
         style={{
-          transform: isInView ? "none" : "translateX(-200px)",
+          transform: isInView ? "none" : "scale(0,1)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          transition: "all 0.5s",
           boxShadow: isInView
           ? 'rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
           : "none",
@@ -27,6 +27,24 @@ const motivation = () => {
           demonstration of fetching, cleaning and visualizing data from a FHIR server.
         </h1>
       </div>
+
+      <motion.div className="w-full flex justify-center items-center py-10"
+          transition={isInView ? {duration:0.7}: {}}
+          animate={isInView ? {opacity: [0,1]} : {opacity: 0}}
+      >
+        {/* explanation */}
+        <h1 className="py-3 text-[1.2rem] main-font">
+          Patient data is fetched from:
+          <a
+            href="https://hapi.fhir.org/baseR4/Patient"
+            target="_blank"
+            className="text-[rgba(40,84,137,1)] border border-black p-2 ml-3 rounded-md ease-in-out duration-200 hover:bg-purple-300"
+          >
+            HERE
+          </a>
+        </h1>
+      </motion.div>
+
     </div>
   );
 };
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { fetchPatient } from '@/helpers/fetcher';
+import React, { useEffect, useState, useRef } from "react";
+import { fetchPatient } from "@/helpers/fetcher";
+
+import { motion, useInView } from "framer-motion"
 
 const Table = () => {
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+
   const [patientData, setPatientData] = useState({
     id: [],
     prefix: [],
@@ -15,7 +20,7 @@ const Table = () => {
     street: [],
     state: [],
     wayOfCommunication: [],
-    valueOfCommunication: []
+    valueOfCommunication: [],
   });
 
   useEffect(() => {
@@ -29,7 +34,7 @@ const Table = () => {
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
   const {
     id,
@@ -42,15 +47,128 @@ const Table = () => {
     street,
     state,
     wayOfCommunication,
-    valueOfCommunication
+    valueOfCommunication,
   } = patientData;
 
-  console.log(id)
+  function td(element, classnames, key) {
+    return (
+      <td className={classnames} key={key}>
+        {element}
+      </td>
+    );
+  }
 
   return (
-    <div>
-      TABLE
-    </div>
+    <motion.div className=" p-20 bg-[#e4e4e4] overflow-x-auto shadow-[0px_35px_50px_rgba(0,0,0,0.2)]"
+    ref={ref}
+    transition={isInView ? {duration:0.7}: {}}
+    animate={isInView ? {opacity: [0,1]} : {opacity: 0}}
+    
+    >
+
+        <table className="rounded-[30px] font-[12px] border border-collapse table-auto ">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                ID
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Prefix
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Family
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                First Name
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Birthday
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Gender
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                City
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Street
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                State
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Way Of Communication
+              </th>
+              <th className="border border-gray-500 px-4 py-2 cursor-pointer main-font">
+                Value Of Communication
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {id.map((element, idx) => (
+              <tr className="items-center" key={'tr'+idx}>
+                {td(
+                  element,
+                  "border h-full border-gray-500 px-4 py-2 bg-gray-200 font-bold text-center main-font",
+                  `${'id' + idx}`
+                )}
+                {td(
+                  prefix[idx],
+                  "border bg-white border-gray-500 px-4 py-2 font-normal  text-center main-font",
+                  `${'prefix'+ idx}`
+                )}
+                {td(
+                  family[idx],
+                  "border bg-white border-gray-500 px-4 py-2 font-normal  text-center main-font",
+                  `${'family' + idx}`
+                )}
+                {td(
+                  firstName[idx],
+                  "border bg-white border-gray-500 px-4 py-2 font-normal  text-center main-font",
+                  `${'firstName' + idx}`
+                )}
+                {td(
+                  bday[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'bday' + idx}`
+                )}
+                {td(
+                  gender[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'gender' + idx}`
+                )}
+                {td(
+                  city[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'city' + idx}`
+                )}
+                {td(
+                  street[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'street' + idx}`
+                )}
+                {td(
+                  state[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'state' + idx}`
+                )}
+                {td(
+                  wayOfCommunication[idx],
+                  "border bg-white border-gray-500  px-4 py-2 font-normal  text-center main-font",
+                  `${'wayOfCommunication' + idx}`
+                )}
+                {td(
+                  valueOfCommunication[idx],
+                  "border bg-white border-gray-500 text-center px-4 py-2 font-normal  text-center main-font",
+                  `${'valueOfCommunication' + idx}`
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </motion.div>
+
   );
 };
 
