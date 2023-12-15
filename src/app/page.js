53e3bb623ec7858990db085c1dfa9a161a12944e
Ghaith AlMasri <almasrighaith101@gmail.com>
2023-12-15 02:29:12 +0100

    fixed footer thing

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Intro from "@/components/intro/Intro.jsx"
 import Motivation from "@/components/motivation/Motivation.jsx"
 import Table from '@/components/table/Table.jsx'
-import Footer from '@/components/Footer/Footer.jsx'
+import Footer from '@/components/footer/Footer.jsx'
 function page() {
   return (
     <div>
